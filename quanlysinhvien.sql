6971b84d15aedc8c5a852e619169561cefff22b5
Phan Minh Thi <thi@corsivalab.space>
2026-02-27 11:18:51 +0700

    add insert into

--- quanlysinhvien.sql
+++ quanlysinhvien.sql
@@ -37,3 +37,22 @@ create table Mark(
     foreign key (StudentId) references Student(StudentId)
 );
 
+insert into Class value(1, 'a1', '2008-12-20', 1);
+insert into Class value(2, 'a2', '2008-12-22', 1);
+insert into Class value(3, 'b3', current_date() , 0);
+
+insert into Student (StudentName, Address, Phone, Status, ClassId) value('Hung', 'Ha Noi', '123456789', 1, 1);
+insert into Student (StudentName, Address, Status, ClassId) value('Hoa', 'Hai Phong',  1, 1);
+insert into Student (StudentName, Address, Phone, Status, ClassId) value('Manh', 'HCM', '147853648', 0, 1);
+
+insert into Subject value(1, 'cf', 5,1), 
+(2, 'c', 6,1),
+(3, 'hdj', 5,1),
+(4, 'rdbms', 10,1);
+
+insert into Mark (SubId, StudentId, Mark, ExamTimes)
+value(1,1,8,1),
+(1,2,10,2),
+(2,1,12,1);
+
+select * from Subject;
